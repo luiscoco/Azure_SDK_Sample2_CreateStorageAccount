986df5d9be3db24eb856ee9ac1ccaff605abd751
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-11-24 12:15:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure;
+using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage;
+using Azure.ResourceManager.Storage.Models;
+
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+string rgName = "myRgNameLUISCOCO";
+AzureLocation location = AzureLocation.WestEurope;
+ArmOperation<ResourceGroupResource> operation = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, new ResourceGroupData(location));
+ResourceGroupResource resourceGroup = operation.Value;
+Console.WriteLine(resourceGroup.Data.Name);
+
+//first we need to define the StorageAccountCreateParameters
+StorageSku sku = new StorageSku(StorageSkuName.StandardGrs);
+StorageKind kind = StorageKind.Storage;
+StorageAccountCreateOrUpdateContent parameters = new StorageAccountCreateOrUpdateContent(sku, kind, location);
+//now we can create a storage account with defined account name and parameters
+StorageAccountCollection accountCollection = resourceGroup.GetStorageAccounts();
+string accountName = "myaccountluiscoco";
+ArmOperation<StorageAccountResource> accountCreateOperation = await accountCollection.CreateOrUpdateAsync(WaitUntil.Completed, accountName, parameters);
+StorageAccountResource storageAccount = accountCreateOperation.Value;
+Console.WriteLine(storageAccount.Data.Name);
